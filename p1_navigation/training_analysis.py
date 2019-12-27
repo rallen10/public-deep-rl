106a4abc9e0c6c127e6f54f1677648d747de6646
Ross Allen <ross.emerson.allen@gmail.com>
2019-12-27 13:06:37 -0500

    using quantiles instead of std

--- p1_navigation/training_analysis.py
+++ p1_navigation/training_analysis.py
@@ -21,11 +21,17 @@ def plot_results(scores_list, window_len=100):
         
         # compute moving average and standard deviation
         mv_avg = np.asarray([np.mean(scores[max(0, i-window_len):i]) for i in range(len(scores))])
-        mv_std = np.asarray([np.std(scores[max(0, i-window_len):i]) for i in range(len(scores))])
+        # mv_std = np.asarray([np.std(scores[max(0, i-window_len):i]) for i in range(len(scores))])
+        mv_q16 = np.asarray([np.quantile(scores[max(0, i-window_len):i], 0.16) for i in range(1,len(scores))])
+        mv_q84 = np.asarray([np.quantile(scores[max(0, i-window_len):i], 0.84) for i in range(1,len(scores))])
+        mv_q16 = np.insert(mv_q16, 0, 0.0)
+        mv_q84 = np.insert(mv_q84, 0, 0.0)
+
 
         # plot
         ax.plot(np.arange(len(scores)), mv_avg)
-        ax.fill_between(np.arange(len(scores)), mv_avg-mv_std, mv_avg+mv_std, alpha=0.3)
+        # ax.fill_between(np.arange(len(scores)), mv_avg-mv_std, mv_avg+mv_std, alpha=0.3)
+        ax.fill_between(np.arange(len(scores)), mv_q16, mv_q84, alpha=0.3)
 
     plt.ylabel('Score')
     plt.xlabel('Episode #')
